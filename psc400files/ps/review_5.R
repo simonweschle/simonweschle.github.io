e5c5282654099a2990757839466d0b7d3f430b01
simonweschle <simon.weschle@gmail.com>
2022-03-04 09:56:05 -0500

    HW 3

---
+++ psc400files/ps/review_5.R
@@ -0,0 +1,24 @@
+
+## you have to write code below every line that starts with #. Lines that start with ##provide explanations
+
+
+# clear the workspace
+
+# load the quality of government dataset (qog_bas_cs_jan22.csv)
+
+# from the variable p_polity2, create a new variable called democracy that takes the value of 1 if p_polity2 is 6 or larger, and zero otherwise (codebook: https://www.qogdata.pol.gu.se/data/codebook_bas_jan22.pdf)
+
+# The outcome variable of interest is female literacy rate (wdi_litradf). compute the difference-in-means of wdi_litradf between democracies and non-democracies
+
+# Run a regression of wdi_litradf on democracy. Save the regression to the object fit and print that object.
+
+
+
+
+# The dependent variable is female literacy rate (wdi_litradf), and our main indepednent variable is 
+
+
+# Now, add the variable vdem_gender (Women political empowerment index) as an additional control variable to the regression (saving it as fit_2var) and print that new model.
+
+
+# finally, add two more variables of your choice as additional controls to the regression (saving it as fit_4var) and print that new model.
